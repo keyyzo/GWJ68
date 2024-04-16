c85d342e2eca9f7808c35ba76a8f607b375cbd9b
keyyzo <58720632+keyyzo@users.noreply.github.com>
2024-04-16 22:16:21 +0100

    simple movement implemented to start off

---
+++ Scripts/player.gd
@@ -0,0 +1,30 @@
+class_name PlayerController
+
+extends CharacterBody2D
+
+
+@export var base_speed : float = 300.0
+@export var acceleration : float = 50.0
+@export var deceleration : float = 65.0
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("exit"):
+		get_tree().quit()
+
+func _physics_process(delta: float) -> void:
+	
+	# Movement inputs
+	
+	var input_vector = Vector2(Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+							Input.get_action_strength("move_down") - Input.get_action_strength("move_up"))
+	
+	input_vector = input_vector.normalized()
+	
+	if input_vector != Vector2.ZERO:
+		velocity += input_vector * acceleration
+		velocity = velocity.limit_length(base_speed)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, deceleration)
+	
+	move_and_slide()
